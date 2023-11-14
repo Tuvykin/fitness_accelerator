27ba77c2017db328b735c40d7e9d5e01b3a041b6
nikolay tuvykin <tuvykin93@yandex.ru>
2023-11-14 11:28:18 +0300

    добавляет сломанные преимущества

--- source/js/modules/cards.js
+++ source/js/modules/cards.js
@@ -2,19 +2,23 @@ const buttons = document.querySelectorAll('.subscription__button');
 const cards = document.querySelector('.cards');
 const textBlock = cards.querySelectorAll('.cards__text');
 const priceBlock = cards.querySelectorAll('.cards__price');
+const cardText = cards.querySelectorAll('.cards__description--text');
 
 
 const subscriptionButtons = [{
   button: buttons[0],
   prices: [5000, 1700, 2700],
+  texts: ['12 занятий'],
 },
 {
   button: buttons[1],
   prices: [30000, 10200, 16200],
+  texts: ['72 занятия'],
 },
 {
   button: buttons[2],
   prices: [60000, 20400, 32400],
+  texts: ['144 занятия'],
 }
 ];
 
@@ -23,11 +27,14 @@ function setActiveButton(btn) {
   btn.classList.add('subscription__button--current');
 }
 
-const setSubscriptionPrice = ({button, prices}) => {
+const setSubscriptionPrice = ({button, prices, texts}) => {
   button.addEventListener('click', () => {
     prices.forEach((price, i) => {
       priceBlock[i].textContent = price;
       textBlock[i].textContent = price;
+    });
+    texts.forEach((text, i) => {
+      cardText[i].textContent = text;
     });
     setActiveButton(button);
   });
--- source/js/modules/gym-video.js
+++ source/js/modules/gym-video.js
@@ -1,10 +1,10 @@
 const ANIMATION_DELAY = 450;
 
 const gymVideoSRC = 'https://www.youtube.com/embed/9TZXsZItgdw?si=UbuakXrvJ-3MeEgS?feature=oembed&autoplay=1&controls=0&showinfo=0&modestbranding=1';
-const videoWrapper = document.querySelector('.gym__wrapper-video');
-const posterWrapper = videoWrapper.querySelector('.poster');
-const posterButton = videoWrapper.querySelector('.poster__button');
-const videoIframe = videoWrapper.querySelector('.gym__iframe');
+const videoWrapper = document.querySelector('[data-wrapper-video]');
+const posterWrapper = videoWrapper.querySelector('[data-poster]');
+const posterButton = videoWrapper.querySelector('[data-poster-button]');
+const videoIframe = videoWrapper.querySelector('[data-gym-iframe]');
 
 function onClickVideoButton() {
   posterWrapper.classList.add('poster__wrapper--hidden');
